f7086ba639cc32e5387b0fdea87e7a98af1c71bd
Zhieniannn <49940771+Zhieniannn@users.noreply.github.com>
2020-05-22 16:02:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
+
+import random
+alphabet = "абвгдежзиклмнопрстуфхцчшщыьэюя"
+
+
 def txt_read1():
     encrypt = ''
-    with open(filename1, 'r', encoding='utf-8') as f:
+    with open('test.txt', 'r', encoding='utf-8') as f:
         encrypt = encrypt.join(f)
+        encrypt = encrypt.replace(' ', 'ъ')
+        ''' .replase Возвращает копию строки, в которой заменены все вхождения указанной строки указанным значением. '''
     return encrypt
 
+def generate_key():
+    alphabet_indexes = list(range(len(alphabet)))#создаем сисок элементов алфавита из строки
+    random.shuffle(alphabet_indexes) # перемешиваем список
+    key = ''.join([alphabet[i] for i in alphabet_indexes])# объеденям список в строку
+    with open('key.txt', 'w', encoding='utf8') as f: 
+        '''range(len()) используются вместе при итерации по изменяемой последовательности
+        для доступа к каждому элементу в последовательности по индексу'''
+        for i in range(len(alphabet)):
+            f.write(f'{alphabet[i]} --> {key[i]}\n')
+        f.write('[] --> ъ')
+    return key
+'''ищем ищешь индекс в алфавите и потом подставляем его в ключ'''
 def encryption(encrypt, key):
-    encrypted = ""
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    for letter in encrypt.lower():
-        if letter in alphabet:
-            position = alphabet.find(letter)
-            newPosition = (position + key) % len(alphabet)
-            encrypted += alphabet[newPosition] 
-        else:
-            encrypted += letter
-    return encrypted
+    text = list(encrypt.lower())
+    for i in range(len(text)):
+        if text[i] in alphabet:
+            index_in_alphabet = alphabet.index(text[i])
+            text[i] = key[index_in_alphabet]
+    return ''.join(text) # .join объеденяет список элементов в одну строку
 
-def text_write(encrypted):
-	with open(filename2, 'w', encoding='utf-8') as f:
-		f.write(encrypted)
+def text_write(encrypt):
+    with open('test1.txt', 'w', encoding='utf-8') as f:
+        f.write(encrypt)
 
-def txt_read2():
-	encrypt = ''
-	with open(filename2, 'r', encoding='utf-8') as f:
-		encrypt = encrypt.join(f)
-	return encrypt
-
+'''ищем индекс в ключе и подставляем его в алфавит'''
 def decryption(encrypt, key):
-	encryptedd = ""
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	for letter in encrypt.lower():
-		if letter in alphabet:
-			position = alphabet.index(letter)
-			newPosition = (position - key) % len(alphabet)
-			encryptedd += alphabet[newPosition]
-		else:
-			encryptedd += letter
-	return encryptedd
+    text = list(encrypt.lower())
+    for i in range(len(text)):
+        if text[i] in key:
+            index_in_key = key.index(text[i])
+            text[i] = alphabet[index_in_key]
+    return ''.join(text) 
 
-def text_write2(encryptedd):
-    with open(filename3, 'w', encoding='utf8') as f:
-        f.write(encryptedd)
-
-
-filename1 = input('Какой файл зашифровать?: ')
-filename2 = input('В какой файл разместить зашифрованный текст?: ')
-filename3 = input('В какой файл разместить расшифрованный текст?: ')
-key = int(input('Введите ключ(от 1 до 25): ')) 
+def decryption_write(encrypt):
+    with open('test2.txt', 'w', encoding='utf-8') as f:
+        encrypt = encrypt.replace('ъ', ' ')
+        f.write(encrypt)
 
 encrypt =  txt_read1()
+key = generate_key()
 encrypt = encryption(encrypt, key)
 text_write(encrypt)
-
-encrypt = txt_read2()
 encrypt = decryption(encrypt, key)
-text_write2(encrypt)
-print("Операция произошла успешна!")
+decryption_write(encrypt)
+
+print("Программа завершена")
